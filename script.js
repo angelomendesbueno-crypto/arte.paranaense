92b0ef4e59554ad5ced32d3cb1e468f0715608ee
angelomendesbueno-crypto <angelo.mendes.bueno@escola.pr.gov.br>
2026-09-23 08:54:36 -0300

    salve

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const botoesCurtir = document. querySelectorAll(".curtir");
-botoesCurtir.fortach (function(botaoCurtir){
-    let curtiu = false;
-    botaoCurtir.
+const botoeCurtir = document. querySelectorAll(".curtir");
+botoeCurtir.fortach (function(botaoCurtir){
+    let curtir = false;
+    botaoCurtir.addEventListener("click",curtir);
+function curtir(){
+        const contador = botaoCurtir.querySelector("span");
+        if(curtir === false)
+    }
 }
